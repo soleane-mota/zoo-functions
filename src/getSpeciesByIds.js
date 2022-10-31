13dd76eacbe3feeb570639ee600506388f5799f6
Soleane Mota <sem.sol@gmail.com>
2022-10-31 16:08:38 -0400

    Resolucao do requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-
+function getSpeciesByIds(...ids) {
+  const speciesId = [];
+  data.species.forEach((animal) => {
+    ids.forEach((id) => (id === animal.id ? speciesId.push(animal) : null));
+  });
+  return speciesId;
 }
 
 module.exports = getSpeciesByIds;
