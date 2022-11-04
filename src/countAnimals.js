a790e794c4c1e1dbdd0037845c233f16fa8b16ee
Soleane Mota <sem.sol@gmail.com>
2022-11-04 14:43:17 -0400

    Resolucao do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const specie = {};
+  species.forEach(({ name, residents }) => {
+    specie[name] = residents.length;
+  });
+  if (animal !== undefined) {
+    const animalData = species.find(({ name }) => name.includes(animal.specie));
+    if (animal.sex === 'male') {
+      const male = animalData.residents.filter(({ sex }) => sex === 'male').length;
+      return male;
+    }
+    if (animal.sex === 'female') {
+      const female = animalData.residents.filter(({ sex }) => sex === 'female').length;
+      return female;
+    }
+    return animalData.residents.length;
+  }
+  return specie;
 }
+
+// console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
