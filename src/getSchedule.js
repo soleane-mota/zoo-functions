91c662e6a2c8a42797efdca194cf1658f419376f
Soleane Mota <sem.sol@gmail.com>
2022-11-14 18:13:10 -0400

    Resolucao do requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+
+function messegerHours(day) {
+  if (hours[day].open !== 0) {
+    return `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+  }
+  return 'CLOSED';
+}
+
+function exhibitionCondition(day) {
+  const exhibition = species.filter((specie) => specie.availability
+    .includes(`${day}`)).map((animal) => animal.name);
+  if (exhibition.length === 0) {
+    return 'The zoo will be closed!';
+  }
+  return exhibition;
+}
+function hoursForEachDay() {
+  const hoursForEachAnimal = {};
+  Object.keys(hours).forEach((day) => {
+    hoursForEachAnimal[day] = {
+      officeHour: messegerHours(day),
+      exhibition: exhibitionCondition(day),
+    };
+  });
+  return hoursForEachAnimal;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget && species.some((specie) => specie.name === scheduleTarget)) {
+    return species.find((specie) => specie.name === scheduleTarget).availability;
+  }
+  if (scheduleTarget && Object.keys(hours).includes(scheduleTarget)) {
+    return { [scheduleTarget]: hoursForEachDay()[scheduleTarget] };
+  }
+  return hoursForEachDay();
 }
 
 module.exports = getSchedule;
